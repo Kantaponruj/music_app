e925570630e26fbafcb4580b657d71f1d4ce9609
Kantapon Rujijanakul <kantaponruj@gmail.com>
2025-02-08 13:52:16 +0700

    Update title

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Music App',
       theme: customTheme(),
       home: const HomePage(),
     );
